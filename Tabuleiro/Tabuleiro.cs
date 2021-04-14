d1a8999adec79e0f2c53a7a8525b890ad73057cd
Lorenzo Rigon <lorenzocardoso@hotmail.com>
2021-04-14 15:35:30 -0300

    Tratando erros com execao personalizada

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -10,7 +10,7 @@
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linhas, colunas]; 
+            pecas = new Peca[linhas, colunas];
         }
 
         public Peca peca(int linha, int coluna)
@@ -18,10 +18,46 @@
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
-            pecas[pos.Linha, pos.Coluna] = p;
-            p.posicao = pos;
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
+            else
+            {
+                pecas[pos.Linha, pos.Coluna] = p;
+                p.posicao = pos;
+            }
+            
         }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+                return false;
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inváldia");
+            }
+
+        }
+
     }
 }
